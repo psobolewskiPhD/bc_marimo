0b9a90ba0f1ca6d979051655d7ed58ee4be14baf
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2026-09-04 10:11:48 -0400

    Initial port over to marimo

--- template/after.sh
+++ template/after.sh
@@ -1,11 +1,11 @@
-# Wait for the QuPath xpra to start
-echo "Waiting for QuPath xpra to open port ${port}..."
+# Wait for the marimo to start
+echo "Waiting for marimo to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "${host}:${port}" 60; then
-  echo "Discovered QuPath xpra listening on port ${port}!"
+  echo "Discovered marimo listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
-  echo "Timed out waiting for QuPath xpra to open port ${port}!"
+  echo "Timed out waiting for marimo to open port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
   pkill -P ${SCRIPT_PID}
   clean_up 1
